c56d05a955852b6ea9e2e38889f53cadb40833a9
IanEKu <ianeddyk@gmail.com>
2023-02-02 08:25:43 +0700

    Bikin layout products dulu

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-})
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+var swiper = new Swiper(".cards-swiper", {
+  slidesPerView: 1,
+  direction: "vertical",
+  centeredSlides: false,
+  dragable: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
